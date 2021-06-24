a331f8dc505621f1257fba9e3d22c28650a5c5ec
lixuefeigithub <lixuefei@live.com>
2021-06-24 17:47:14 -0400

    Update ExpressionExtensions.cs

--- DevLibrary/ReflectionLibrary/ExpressionExtensions.cs
+++ DevLibrary/ReflectionLibrary/ExpressionExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -18,6 +18,40 @@ namespace ReflectionLibrary
             }
 
             return null;
+        }
+
+        public static PropertyInfo GetPropertyInfo<TSource, TProperty>(this Expression<Func<TSource, TProperty>> propertyLambda)
+        {
+            Type type = typeof(TSource);
+
+            MemberExpression member = propertyLambda.Body as MemberExpression;
+
+            if (member == null)
+            {
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    propertyLambda.ToString()));
+            }
+
+            PropertyInfo propInfo = member.Member as PropertyInfo;
+
+            if (propInfo == null)
+            {
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    propertyLambda.ToString()));
+            }
+
+            if (type != propInfo.ReflectedType &&
+                !type.IsSubclassOf(propInfo.ReflectedType))
+            {
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property that is not from type {1}.",
+                    propertyLambda.ToString(),
+                    type));
+            }
+
+            return propInfo;
         }
     }
 }
