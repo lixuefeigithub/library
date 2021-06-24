3d2dc5b1db2c06efdf087dcf2ff601a50da6466e
lixuefeigithub <lixuefei@live.com>
2021-06-24 17:41:39 -0400

    Add files via upload

---
+++ DevLibrary/ReflectionLibrary/ExpressionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ReflectionLibrary
+{
+    public static class ExpressionExtensions
+    {
+        public static PropertyInfo GetPropertyInfo(this LambdaExpression propertyLambdaExpression)
+        {
+            var memberSelectorExpression = propertyLambdaExpression.Body as MemberExpression;
+
+            if (memberSelectorExpression != null)
+            {
+                var property = memberSelectorExpression.Member as PropertyInfo;
+
+                return property;
+            }
+
+            return null;
+        }
+    }
+}
