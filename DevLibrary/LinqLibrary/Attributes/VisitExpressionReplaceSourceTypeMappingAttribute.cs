f8b84e0899765704372cbf69d03dd7590d6c3ed1
Xuefei Li <x.li@gothammgt.ca>
2022-12-28 10:08:26 -0800

    Improve 2022 Dec 28

---
+++ DevLibrary/LinqLibrary/Attributes/VisitExpressionReplaceSourceTypeMappingAttribute.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqLibrary.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    public class VisitExpressionReplaceSourceTypeMappingAttribute : Attribute
+    {
+        public Type TargetType { get; set; }
+        public string TargetTypePropertyName { get; set; }
+
+        public bool IsMatchTargetType(Type realTargetType)
+        {
+            if (TargetType == null)
+            {
+                return false;
+            }
+
+            if (realTargetType == null)
+            {
+                return false;
+            }
+
+            if (TargetType.IsAssignableFrom(realTargetType))
+            {
+                return true;
+            }
+
+            if (realTargetType.IsGenericType && TargetType.IsAssignableFrom(realTargetType.GetGenericTypeDefinition()))
+            {
+                return true;
+            }
+
+            if (TargetType.IsInterface)
+            {
+                var entityTypeInterfaces = realTargetType.GetInterfaces();
+
+                if (entityTypeInterfaces.Any(i => TargetType.IsAssignableFrom(i)))
+                {
+                    return true;
+                }
+
+                if (entityTypeInterfaces.Any(i => i.IsGenericType && TargetType.IsAssignableFrom(i.GetGenericTypeDefinition())))
+                {
+                    return true;
+                }
+            }
+
+            if (!TargetType.IsInterface && TargetType.IsGenericType)
+            {
+                var baseType = realTargetType.BaseType;
+
+                while (baseType != null && baseType != typeof(object))
+                {
+                    if (baseType.IsGenericType && TargetType.IsAssignableFrom(baseType.GetGenericTypeDefinition()))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
